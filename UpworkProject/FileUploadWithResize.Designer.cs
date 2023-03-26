8a765c078e7232005576efd82f06ef533d0e27c5
alvin maulana <alvin.maulana@ccn.com.sg>
2023-03-27 00:58:23 +0800

    Add project files.

---
+++ UpworkProject/FileUploadWithResize.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace UpworkProject
+{
+    partial class FileUploadWithResize
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.filename = new System.Windows.Forms.Label();
+            this.Extension = new System.Windows.Forms.Label();
+            this.Size = new System.Windows.Forms.Label();
+            this.OrgPath = new System.Windows.Forms.Label();
+            this.compressed = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Upload Here";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(47, 104);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(368, 203);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Image Preview";
+            // 
+            // filename
+            // 
+            this.filename.AutoSize = true;
+            this.filename.Location = new System.Drawing.Point(449, 104);
+            this.filename.Name = "filename";
+            this.filename.Size = new System.Drawing.Size(55, 13);
+            this.filename.TabIndex = 3;
+            this.filename.Text = "Filename: ";
+            // 
+            // Extension
+            // 
+            this.Extension.AutoSize = true;
+            this.Extension.Location = new System.Drawing.Point(449, 135);
+            this.Extension.Name = "Extension";
+            this.Extension.Size = new System.Drawing.Size(53, 13);
+            this.Extension.TabIndex = 4;
+            this.Extension.Text = "Extension";
+            // 
+            // Size
+            // 
+            this.Size.AutoSize = true;
+            this.Size.Location = new System.Drawing.Point(449, 187);
+            this.Size.Name = "Size";
+            this.Size.Size = new System.Drawing.Size(27, 13);
+            this.Size.TabIndex = 5;
+            this.Size.Text = "Size";
+            // 
+            // OrgPath
+            // 
+            this.OrgPath.AutoSize = true;
+            this.OrgPath.Location = new System.Drawing.Point(449, 162);
+            this.OrgPath.Name = "OrgPath";
+            this.OrgPath.Size = new System.Drawing.Size(67, 13);
+            this.OrgPath.TabIndex = 6;
+            this.OrgPath.Text = "Original Path";
+            // 
+            // compressed
+            // 
+            this.compressed.AutoSize = true;
+            this.compressed.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.compressed.Location = new System.Drawing.Point(449, 215);
+            this.compressed.Name = "compressed";
+            this.compressed.Size = new System.Drawing.Size(45, 25);
+            this.compressed.TabIndex = 7;
+            this.compressed.Text = "123";
+            this.compressed.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(961, 391);
+            this.Controls.Add(this.compressed);
+            this.Controls.Add(this.OrgPath);
+            this.Controls.Add(this.Size);
+            this.Controls.Add(this.Extension);
+            this.Controls.Add(this.filename);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Upwork Project";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label filename;
+        private System.Windows.Forms.Label Extension;
+        private System.Windows.Forms.Label Size;
+        private System.Windows.Forms.Label OrgPath;
+        private System.Windows.Forms.Label compressed;
+    }
+}
+
---
+++ UpworkProject/FileUploadWithResize.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpworkProject
+{
+    public partial class FileUploadWithResize : Form
+    {
+        public FileUploadWithResize()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                //check image validation
+                if (!CheckIfImage(openFileDialog1.FileName))
+                {
+                    MessageBox.Show("File is not image!");
+                    return;
+                }
+
+                var fileNameTxt = openFileDialog1.FileName;
+                var size = new FileInfo(openFileDialog1.FileName).Length;
+                var extension = Path.GetExtension(openFileDialog1.FileName);
+
+                //UI
+                filename.Text = "Filename: " + openFileDialog1.SafeFileName;
+                Size.Text = "Size: " + SizeSuffix(size, 2);
+                Extension.Text = "Extension: " + extension;
+                OrgPath.Text = "Original Path: " + openFileDialog1.FileName;
+
+                //Picturebox handling
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                //if size > 1mb
+                if (size > 1048576)
+                {
+                    var image = new Bitmap(fileNameTxt);
+                    Image i = resizeImage(image, new Size(985, 445));
+                    pictureBox1.Image = i;
+                    compressed.Text = "Compressed! - New size: " + SizeSuffix(CheckSize(i),2);
+                    compressed.Visible = true;
+                } else
+                {
+                    var image = new Bitmap(fileNameTxt);
+                    pictureBox1.Image = image;
+                    compressed.Text = "";
+                    compressed.Visible = false;
+                }
+
+                //Save image to server
+                //Save image to db
+            }
+        }
+
+         readonly string[] SizeSuffixes =
+                   { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+
+         string SizeSuffix(Int64 value, int decimalPlaces = 1)
+        {
+            if (decimalPlaces < 0) { throw new ArgumentOutOfRangeException("decimalPlaces"); }
+            if (value < 0) { return "-" + SizeSuffix(-value, decimalPlaces); }
+            if (value == 0) { return string.Format("{0:n" + decimalPlaces + "} bytes", 0); }
+
+            // mag is 0 for bytes, 1 for KB, 2, for MB, etc.
+            int mag = (int)Math.Log(value, 1024);
+
+            // 1L << (mag * 10) == 2 ^ (10 * mag) 
+            // [i.e. the number of bytes in the unit corresponding to mag]
+            decimal adjustedSize = (decimal)value / (1L << (mag * 10));
+
+            // make adjustment when the value is large enough that
+            // it would round up to 1000 or more
+            if (Math.Round(adjustedSize, decimalPlaces) >= 1000)
+            {
+                mag += 1;
+                adjustedSize /= 1024;
+            }
+
+            return string.Format("{0:n" + decimalPlaces + "} {1}",
+                adjustedSize,
+                SizeSuffixes[mag]);
+        }
+
+        private static Image resizeImage(Image imgToResize, Size size)
+        {
+            //Get the image current width  
+            int sourceWidth = imgToResize.Width;
+            //Get the image current height  
+            int sourceHeight = imgToResize.Height;
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+            //Calulate  width with new desired size  
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            //Calculate height with new desired size  
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+            //New Width  
+            int destWidth = (int)(sourceWidth * nPercent);
+            //New Height  
+            int destHeight = (int)(sourceHeight * nPercent);
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((System.Drawing.Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            // Draw image with new width and height  
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+            return (System.Drawing.Image)b;
+        }
+
+        private long CheckSize(Image image)
+        {
+            long jpegByteSize;
+            using (var ms = new MemoryStream()) // estimatedLength can be original fileLength
+            {
+                image.Save(ms, ImageFormat.Jpeg); // save image to stream in Jpeg format
+                jpegByteSize = ms.Length;
+            }
+
+            return jpegByteSize;
+        }
+
+        private bool CheckIfImage(string filePath)
+        {
+            bool result = false;
+
+            List<string> ImageExtensions = new List<string> { ".JPG", ".JPEG", ".JPE", ".BMP", ".GIF", ".PNG" };
+
+            if (ImageExtensions.Contains(Path.GetExtension(filePath).ToUpperInvariant()))
+                result = true;
+
+            return result;
+        }
+    }
+}
